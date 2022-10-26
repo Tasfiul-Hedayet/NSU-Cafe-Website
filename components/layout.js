c9c85f374baa78d7d4b5e1a849e397e67583c286
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-10-27 04:57:52 +0600

    component file

---
+++ components/layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header'
+
+import Footer from './Footer'
+
+const layout = () => {
+  return (
+    <div>
+        <head>
+            <title>
+                NSU Cafe
+            </title>
+        </head>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default layout
