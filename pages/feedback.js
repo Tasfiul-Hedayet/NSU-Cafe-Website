86ca0e4992675f11a34fb18fe803c754ec56a967
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-11-01 03:48:25 +0600

    new update

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,14 +1,51 @@
+import Link from 'next/link'
 import Image from 'next/image'
+import styles from "../styles/feedback.module.css";
 
-const Feedback = () => {
+
+function Feedback() {
   return (
     <>
-    <h1>Subscription </h1>
-    <div className = "logo">
-    <Image src = "/feedback.png" width={300} height={200} />
-    <button className="btn p-20"> <a href="/payment">Payment Page</a></button>
 
+    {/* image part */}
+
+    <div className = {styles.logo}>
+    <Image src = "/feedback.png" width={200} height={100} />
     </div>
+
+
+    {/* Page tile */}
+    <div className={styles.title}> <h1>Feedback form</h1></div>
+   
+
+
+    {/* drop down */}
+   
+
+
+
+
+    {/* text box section */}
+
+    <form action="/send-data-here" method="post">
+
+    <label for="first">Write down your review:</label>
+
+    <p  className={styles.p1}>
+      <input type="text" id="first" name="first" /></p>
+
+    </form>
+
+
+
+    {/* submit button section */}
+    <div>
+      
+    <button className="btn p+20"> <Link href="/message">Submit</Link> </button>
+    
+    </div>
+
+
     </>
   )
 }
---
+++ pages/message.js
@@ -0,0 +1,23 @@
+
+import Link from "next/link";
+import styles from "../styles/message.module.css";
+
+
+function message(){
+
+  return (
+    <>
+    
+        <h1 className={styles.h1}>Thank you for your valueable review. </h1>
+    
+    <div>
+        <button className={styles.button}> <Link href="/">Return to Home</Link> </button>
+    </div>
+    
+        
+
+    </>
+  )
+}
+
+export default message;
