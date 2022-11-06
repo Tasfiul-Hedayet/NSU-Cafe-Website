ab13bdb72277065d074242cc62e632b05917a66e
Abir Anjum Prottoy <62479082+f0rGoT-Ten@users.noreply.github.com>
2022-11-06 15:15:59 +0600

    tailwind

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import Link from 'next/link'
 import Header from './Header'
 
 import Footer from './Footer'
 
-const layout = () => {
+function layout () {
   return (
-    <div>
-        <head>
-            <title>
-                NSU Cafe
-            </title>
-        </head>
-        <Header/>
-        <Footer/>
-    </div>
+    <> 
+<nav className='shadow-md flex flex-wrap justify-center items-center p-4 mb-10 sticky top-0 z-50 bg-white'>
+        <ul className='flex'>
+            <Link href='/'><li className='mx-3 cursor-pointer'>Home</li></Link>
+            <Link href='/about'><li className='mx-3 cursor-pointer'>About</li></Link>
+            <Link href='/blog'><li className='mx-3 cursor-pointer'>Blogs</li></Link>
+            <Link href='/contact'><li className='mx-3 cursor-pointer'>Contact Us</li></Link>
+        </ul>
+    </nav>
+    </>
   )
 }
 
