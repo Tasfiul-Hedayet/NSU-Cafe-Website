99c0f22776ee129604105b28565a65be6963abe7
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-11-11 12:02:45 +0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,4 @@
-import 'tailwindcss/base';
 
-import './nprogress.css';
-import './base.css';
-import './scrollbar.css';
-import './fast-image-zoom.css';
-import './grid.css';
-import './anchor.css';
-import './prism.css';
-import './gif.css';
-import './giscus.css';
-
-import 'tailwindcss/components';
-import 'tailwindcss/utilities';
 import Image from 'next/image';
 
 
