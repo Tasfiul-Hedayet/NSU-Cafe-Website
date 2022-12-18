51628dfc5c57d97cfe513fae629f9ec5069810d6
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-12-18 15:45:05 +0600

    up

--- pages/payment.js
+++ pages/payment.js
@@ -1,21 +1,36 @@
 import Link from "next/link";
-import Image from "next/image";
-import Navbar from "../components/Nav"
-import Footer from "../components/Footer"
+import Navbar from "../components/Nav";
+import Footer from "../components/Footer";
 
 function payment() {
   return (
     <>
-      <Navbar/>
-      <h1>Subscription </h1>
-      <div className="logo">
-        <Image src="/pay.jpg" width={300} height={200} />
-      </div>
-      <button>
-        {" "}
-        <Link href="/gateway">Purchase</Link>
-      </button>
-      <Footer/>
+      <Navbar />
+      <div className="main">
+        <div class="background">
+          <div class="auth-box">
+            <h2>Card Details</h2>
+            <div class="flex">
+              <p>First Name</p>
+              <input type="text" />
+              <p>Last Name</p>
+              <input type="text" />
+            </div>
+
+            <div class="flex">
+              <p>Card No</p>
+              <input type="text" />
+              <p>CVV</p>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+        <div class="rounded-full border-2 hover:border-t-4">
+       <Link href="./">Go back</Link>  
+        </div>
+
+</div>
+      <Footer />
     </>
   );
 }
