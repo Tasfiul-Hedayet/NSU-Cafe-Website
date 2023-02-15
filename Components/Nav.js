9c3d8733aa24bc94e323f6bb98c61ad2e99a6139
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2023-02-15 21:31:47 +0600

    title color changed

--- Components/Nav.js
+++ Components/Nav.js
@@ -6,7 +6,7 @@ function Navbar () {
         <nav className='shadow-md flex flex-wrap justify-center items-center p-4 mb-10 sticky top-0 z-50 bg-white'>
         <ul className='flex'>
             <Link href='/'><li className='mx-3 cursor-pointer'>Home</li></Link>
-            <Link href='/foodmenu'><li className='mx-3 cursor-pointer'>Foodmenu</li></Link>
+            {/* <Link href='/foodmenu'><li className='mx-3 cursor-pointer'>Foodmenu</li></Link> */}
             {/* <Link href='/payment'><li className='mx-3 cursor-pointer'>Payment</li></Link> */}
             <Link href='/feedback'><li className='mx-3 cursor-pointer'>Feedback</li></Link>
             <Link href='/login'><li className='mx-3 cursor-pointer'>Login</li></Link>
