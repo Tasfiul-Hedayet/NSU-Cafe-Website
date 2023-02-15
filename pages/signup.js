eb09f8e8dec51eeea6a9e9e2b2335f60ba1c79fc
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2023-02-15 21:19:48 +0600

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -12,22 +12,25 @@ function signup() {
         Welcome to NSU Cafe Digitaliztion System
       </h1>
       <h2 className={styles.h2}> Register </h2>
+      <br />
 
       <div className={styles.form}>
         <form action="/send-data-here" method="post">
-          <label for="name"> Name</label>
+          {/* <label for="name"> Name</label>
+          <br /> */}
+
+          <input type="text" className={styles.inbox} name="Name" placeholder="Name" />
+          <br />
           <br />
 
-          <input type="text" className={styles.inbox} name="first" />
+          {/* <label for="email"> Email</label>
+          <br /> */}
+
+          <input type="text" className={styles.inbox} name="email" placeholder="Email" />
           <br />
 
-          <label for="email"> Email</label>
-          <br />
-
-          <input type="text" className={styles.inbox} name="email" />
-          <br />
-
-          <label for="pswrd">Password</label>
+          {/* <label for="pswrd">Password</label>
+          <br /> */}
           <br />
 
           <input
@@ -36,11 +39,13 @@ function signup() {
             name="pswrd"
             pattern="[a-z0-9]{1,15}"
             title="Password should be digits (0 to 9) or alphabets (a to z)."
+            placeholder="password"
           />
           <br />
-          
-          <label for="pswrd">Confirm Password</label>
           <br />
+          
+          {/* <label for="pswrd">Confirm Password</label>
+          <br /> */}
 
           <input
             type="password"
@@ -48,6 +53,7 @@ function signup() {
             name="pswrd"
             pattern="[a-z0-9]{1,15}"
             title="Password should be digits (0 to 9) or alphabets (a to z)."
+            placeholder="Confirm password"
           />
           <br />
           <br />
